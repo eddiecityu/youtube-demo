f892d73eec6f0378ba36b8ade15ef5268dd4a598
Umang Mehta <umang.mehta@affectiva.com>
2017-09-12 10:55:33 -0400

    Updating the demo with the new logo

--- demo/js/run_demo.js
+++ demo/js/run_demo.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     var isChrome = !!window.chrome && !!window.chrome.webstore;
     var isFirefox = typeof InstallTrigger !== 'undefined';
     var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
-    
+
     if (isChrome || isFirefox || isOpera) {
         JSSDKDemo.init();
         JSSDKDemo.run();
@@ -31,7 +31,7 @@ var JSSDKDemo = (function() {
     var start_time = 0;
     var stop_time = 0;
     var playing = false;
-    
+
     var stop_capture_timeout = null;
     var time_left_sec = 0;
     var time_buffering_ms = 0;
@@ -45,36 +45,36 @@ var JSSDKDemo = (function() {
     var y_scale = d3.scale.linear().domain([100, 0]).range([2, 248]);
     var t = null;
     var cursor_interval = null;
-    
+
     var API_KEY = "AIzaSyCdQbLORhF7PGVJ7DG1tkoVJGgDYwA_o0M";
 
     var run = function() {
         var facevideo_node = document.getElementById("facevideo-node");
         detector = new affdex.CameraDetector(facevideo_node);
         detector.detectAllEmotions();
-        
+
         detector.addEventListener("onWebcamConnectSuccess", function() {
             show_message("msg-starting-webcam");
         });
-        
+
         detector.addEventListener("onWebcamConnectFailure", function() {
             show_message("msg-webcam-failure");
         });
-        
+
         if (detector && !detector.isRunning) {
             detector.start();
         }
-        
+
         // get the video element inside the div with id "facevideo-node"
         var face_video = $("#facevideo-node video")[0];
         face_video.addEventListener("playing", function() {
             show_message("msg-detector-status");
         });
-        
+
         detector.addEventListener("onInitializeSuccess", function() {
             show_message("instructions");
         });
-        
+
         detector.addEventListener("onImageResultsSuccess", function(faces, image, timestamp) {
             // get the time as close to the actual time of the frame as possible
             //  account for time spent buffering
@@ -85,7 +85,7 @@ var JSSDKDemo = (function() {
                     face_visible = false;
                     create_alert("no-face", "No face was detected. Please re-position your face and/or webcam.");
                 }
-                
+
                 if (faces.length > 0) {
                     if (!face_visible) {
                         face_visible = true;
@@ -102,22 +102,22 @@ var JSSDKDemo = (function() {
                         processed_frames[idx].push([fake_timestamp, 0]);
                     });
                 }
-                
+
                 update_plot();
             }
         });
     };
-        
+
     var start_button_click = function(event) {
         $(".demo-message").hide();
-        
+
         if (ready_to_accept_input) {
             ready_to_accept_input = false;
             var video_id;
-            
+
             if (event.data == null) {
                 var blob = document.getElementById("start-form").value;
-                
+
                 if (blob === "" || blob.includes("http://") || blob.includes("https://")) { // treat as URL
                     video_id = blob.split("v=")[1] || "";
                     var ampersandPosition = video_id.indexOf("&");
@@ -128,17 +128,17 @@ var JSSDKDemo = (function() {
                     var url = "https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&key=" + API_KEY + "&maxResults=10&safeSearch=strict&q=" + blob;
                     http_get_async(url, add_to_search_results);
                 }
-                
+
             } else { // play the video that was clicked
                 video_id = event.data.id;
             }
-            
+
             if (typeof video_id !== "undefined") {
                 player.loadVideoById(video_id, 0);
             }
         }
     };
-    
+
     var begin_capture = function() {
         // take care of gap at beginning
         x_scale = d3.scale.linear().domain([start_time, start_time + video_duration_ms]).range([0, svg_width]);
@@ -146,38 +146,38 @@ var JSSDKDemo = (function() {
             processed_frames[idx].push([start_time, 0]);
         });
         update_plot();
-        
+
         capture_frames = true;
-        
+
         $("#demo-setup").fadeOut("fast", function() {
             $("#video-container").show();
             init_plot();
             stop_capture_timeout = setTimeout(stop_capture, video_duration_ms);
         });
     };
-    
+
     var stop_capture = function() {
         stop_time = get_current_time_adjusted();
         capture_frames = false;
         detector.stop();
         $(".alert").hide();
-        
+
         // focus on message
         $("#lightbox").fadeIn(750, function() {
             // render cursor
             add_cursor();
             track_video();
-            
+
             // make emotion buttons and player clickable
             //$("#ul-wrapper").css("pointer-events", "");
             $("#player").css("pointer-events", "");
-            
+
             $("#play-again").fadeIn(500, function() {
                 $("#lightbox").one("click", transition_to_playback);
             });
         });
     };
-    
+
     var track_video = function() {
         cursor_interval = setInterval(function() {
             if (playing) {
@@ -186,7 +186,7 @@ var JSSDKDemo = (function() {
             }
         }, 50);
     };
-    
+
     var add_cursor = function() {
         // drag and drop
         var curve = d3.select("#svg-curve");
@@ -197,7 +197,7 @@ var JSSDKDemo = (function() {
         drag_group.call(d3.behavior.drag().on("drag", function() {
             var x_coord = d3.event.x;
             var playback_time = t.invert(x_coord);
-            
+
             if (playback_time < 0) {
                 x_coord = 0;
                 playback_time = 0;
@@ -205,10 +205,10 @@ var JSSDKDemo = (function() {
                 playback_time = video_cutoff_sec - 0.001;
                 x_coord = t(playback_time);
             }
-            
+
             translate_cursor(x_coord);
             player.seekTo(playback_time);
-            
+
         }).on("dragstart", function(event) {
             if (playing) {
                 clearInterval(cursor_interval);
@@ -231,30 +231,30 @@ var JSSDKDemo = (function() {
     var svg_click = function() {
         var x_click = d3.mouse(this)[0];
         var playback_time = t.invert(x_click);
-        
+
         if (playback_time >= video_cutoff_sec) {
             playback_time = video_cutoff_sec - 0.001;
             x_click = t(playback_time);
         }
-        
+
         if (playing) {
             clearInterval(cursor_interval);
         }
-        
+
         translate_cursor(x_click);
         player.seekTo(playback_time);
-        
+
         if (playing) {
             track_video();
         }
     };
-    
+
     var path = d3.svg.line().x(function(d, i) {
         return x_scale(d[0])
     }).y(function(d, i) {
         return y_scale(d[1])
     }).interpolate("basis");
-    
+
     var init_plot = function() {
         var curve = d3.select("#svg-curve");
 
@@ -285,25 +285,25 @@ var JSSDKDemo = (function() {
     var translate_cursor = function(x_coord) {
         // translate timeline cursor
         d3.selectAll("#svg-curve .draggable-group").attr("transform", "translate(" + x_coord + ", 0)");
-        
+
         // render time
         var time = d3.selectAll("#svg-curve text.video_current_time");
         var time_sec = Math.floor(x_coord / svg_width * video_duration_sec);
         var text = text_time(time_sec);
         time.text(text);
-        
+
         // figure out if flip is necessary
         $("#text-width")[0].innerHTML = text;
         var text_width = $("#text-width")[0].clientWidth;
         var flip_at = svg_width - text_width - 5;
-        
+
         if (x_coord > flip_at) {
             time.attr("transform", "translate(" + (x_coord - text_width - 10) + ", 0)");
         } else {
             time.attr("transform", "translate(" + x_coord + ", 0)");
         }
     };
-    
+
     var text_time = function(time_sec) {
         return Math.floor(time_sec / 60) + ":" + ((time_sec % 60 < 10) ? ("0" + time_sec % 60) : time_sec % 60);
     };
@@ -330,12 +330,12 @@ var JSSDKDemo = (function() {
             });
         });
     };
-    
+
     var no_internet = function() {
         $(".alert").hide();
         create_alert("terminated", "It appears that you aren't connected to the Internet anymore. Please refresh the page and try again.");
     };
-    
+
     var http_get_async = function(url, callback) {
         var xmlHttp = new XMLHttpRequest();
         xmlHttp.onreadystatechange = function() {
@@ -346,25 +346,25 @@ var JSSDKDemo = (function() {
         xmlHttp.open("GET", url, true);
         xmlHttp.send(null);
     };
-    
+
     var add_to_search_results = function(text) {
         $("#search-results").empty();
         var results = JSON.parse(text);
         var list = results.items;
-        
+
         // add results
         for (var i = 0; i < list.length; i++) {
             var v = list[i];
             var s = v.snippet;
             var id = v.id.videoId;
-            
+
             var result = document.createElement("div");
             result.className = "list-group-item";
             result.innerHTML = '<table><tr><td><img class="thumbnail" id="' + id + '" src="' + s.thumbnails.medium.url + '" style="margin-right:15px"></td><td valign="top"><h3>' + s.title + '</h3><span>' + s.description + '</span></td></tr></table>';
             $("#search-results").append(result);
             $("#"+id).click({id: id}, start_button_click);
         }
-        
+
         // show a message for when no videos were found
         var num_videos = results.pageInfo.totalResults;
         if (num_videos === 0) {
@@ -373,24 +373,24 @@ var JSSDKDemo = (function() {
             message.innerHTML = '<p>No results were found.</p>';
             $("#search-results").append(message);
         }
-        
+
         // scroll to results
         $("html, body").animate({
             scrollTop: $("#search-results").offset().top - 15
         });
-        
+
         ready_to_accept_input = true;
     };
-    
-    var video_ids = ["ugo7Y2lRsxc", "Pc7BnT5X1tw", "IV_ef2mm4G0", "dlNO2trC-mk", "ugn_qmQ0NFo", "7u6M18M3D0s"];
-    
+
+    var video_ids = ["EglYdO0k5nQ", "z63KGZE4rnM", "IV_ef2mm4G0", "dlNO2trC-mk", "lhzwmYRXPp4", "0kfLd52jF3Y"];
+
     var populate_examples = function() {
         video_ids.forEach(function(element, index) {
             var id = "#example-" + index;
             var thumbnail_url = "https://i.ytimg.com/vi/" + video_ids[index] + "/mqdefault.jpg";
             $(id)[0].style.backgroundImage = "url(" + thumbnail_url + ")";
             $(id).click({id: video_ids[index]}, start_button_click);
-            
+
             var url = "https://www.googleapis.com/youtube/v3/videos?part=snippet&id=" + element + "&key=" + API_KEY;
             http_get_async(url, function(text) {
                 var results = JSON.parse(text);
@@ -407,11 +407,11 @@ var JSSDKDemo = (function() {
             });
         });
     };
-    
+
     var get_current_time_adjusted = function() {
         return Date.now() - time_buffering_ms;
     };
-    
+
     var create_alert = function(id, text) {
         $("#lightbox").fadeIn(500);
         $("<div></div>", {
@@ -423,35 +423,35 @@ var JSSDKDemo = (function() {
         $("#" + id).css({"text-align": "center", "z-index": 2});
         $("#" + id).fadeIn(1000);
     };
-    
+
     var show_message = function(id) {
         $(".demo-message").hide();
         $(document.getElementById(id)).fadeIn("fast");
     };
-    
+
     var fade_and_remove = function(id) {
         $(id).fadeOut(500, function() {
             this.remove();
         });
     };
-    
-    
-    
+
+
+
     return {
         init: function() {
             $("#btn-start").click(start_button_click);
             $("#btn-play-again").one("click", transition_to_playback);
-            
+
             // add click functionality to enter button
             $("#start-form").keyup(function(event) {
                 if (event.keyCode === 13 || event.which === 13) {
                     $("#btn-start").click();
                 }
             });
-            
+
             // "show all" button
             $("#all").css("border", "3px solid #ffcc66");
-            
+
             $("#all").click(function() {
                 // set border
                 if (selected_emotion !== "all") {
@@ -459,17 +459,17 @@ var JSSDKDemo = (function() {
                     $(this).css("border", "3px solid #ffcc66");
                 }
                 selected_emotion = "all";
-                
+
                 var curve = d3.select("#svg-curve");
                 curve.selectAll("path.curve")
                     .transition()
                     .duration(400)
                     .attr("stroke-opacity", 1.0);
             });
-            
+
             // populate sample videos
             populate_examples();
-            
+
             // load IFrame Player API code asynchronously
             setTimeout(function() {
                 var tag = document.createElement("script");
@@ -477,7 +477,7 @@ var JSSDKDemo = (function() {
                 var firstScriptTag = document.getElementsByTagName("script")[0];
                 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
             }, 1000);
-            
+
             // initialize player
             window.onYouTubeIframeAPIReady = function() {
                 player = new YT.Player("player", {
@@ -508,11 +508,11 @@ var JSSDKDemo = (function() {
 
                 function onPlayerStateChange(event) {
                     var status = event.data;
-                    
+
                     if (!finished_watching) {
                         if (status === YT.PlayerState.PLAYING) {
                             video_duration_sec = player.getDuration();
-                            
+
                             if (video_duration_sec > 0) {
                                 if (video_duration_sec > VIDEO_LENGTH_THRESHOLD) {
                                     if (start_time > 0) { // started playing again after buffering
@@ -522,7 +522,7 @@ var JSSDKDemo = (function() {
                                         // add how much time was spent buffering
                                         var buffer_time = Date.now() - buffer_start_time_ms;
                                         time_buffering_ms += buffer_time;
-                                    
+
                                     } else { // just started playing from the beginning
                                         start_time = Date.now();
                                         player.setVolume(VIDEO_VOLUME);
@@ -538,18 +538,18 @@ var JSSDKDemo = (function() {
                                     ready_to_accept_input = true;
                                 }
                             }
-                            
+
                         }
                         else if (status === YT.PlayerState.BUFFERING && video_duration_sec > VIDEO_LENGTH_THRESHOLD) { // video is valid but needs to buffer
                             capture_frames = false;
                             clearTimeout(stop_capture_timeout);
                             time_left_sec = video_duration_sec - player.getCurrentTime();
-                            
+
                             // log the time when buffering started
                             buffer_start_time_ms = Date.now();
                         }
                     }
-                    
+
                     if (status === YT.PlayerState.ENDED) {
                         if (!finished_watching) {
                             finished_watching = true;
@@ -565,7 +565,7 @@ var JSSDKDemo = (function() {
                         detector.stop();
                         no_internet();
                     }
-                    
+
                     // make cursor less buggy while video is paused
                     if (status === YT.PlayerState.PLAYING) {
                         playing = true;
@@ -585,7 +585,7 @@ var JSSDKDemo = (function() {
                 $("#" + clicked_id).css("border", "3px solid #ffcc66");
             }
             selected_emotion = clicked_id;
-            
+
             var curve = d3.select("#svg-curve");
             curve.selectAll("path.curve")
                 .transition()
@@ -598,7 +598,7 @@ var JSSDKDemo = (function() {
                     }
                 });
         },
-        
+
         create_alert: create_alert
     };
 })();
